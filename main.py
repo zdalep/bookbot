4dcd6d3403f6e73c7fe206289e72f5d7f05f50c1
Pup Meat <zdalepayne@gmail.com>
2025-05-31 06:01:41 -0500

    C: Complete!

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-from stats import main
+from stats import word_count, character_count, sort_list
+import sys
+def main():
+    if len(sys.argv) > 1:
+        with open(sys.argv[1])as f:
+            text = f.read()
+            num_words = word_count(text)
+            char_count = character_count(text)
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    sorted_letters = sort_list(char_count)
+    for letter in sorted_letters:
+        print(f"{letter['char']}: {letter['num']}")
+    print("============= END ===============")
+if __name__ == "__main__": main()
--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@ def word_count(text):
     split_contents = text.split()
     count = len(split_contents)
     return count
+
 def character_count(text):
     characters = {}
     lowcase = text.lower()
@@ -11,11 +12,13 @@ def character_count(text):
         else:
             characters[character] += 1
     return characters
-def main():
-    with open("./books/frankenstein.txt")as f:
-        text = f.read()
-        num_words = word_count(text)
-        char_count = character_count(text)
-    print(f"{num_words} words found in the document")
-    print(char_count)
-main()
+def sort_list(dict):
+    results = []
+    for key, value in dict.items():
+        letter = {"char": key, "num": value}
+        if letter["char"].isalpha():
+            results.append(letter)
+    def sort_on(item):
+        return item["num"]
+    results.sort(reverse = True, key=sort_on)
+    return results
